7f37e739634faaa675ede4207d2f41295cdf93ea
janarbet14 <jan.arbet14@gmail.com>
2016-07-28 10:55:09 +0200

    s

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,5 +9,5 @@ use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
 }
